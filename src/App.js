e07865f8c242f5d4e40b7423ef1dae8cf1d58b29
seung yeon <zionimobu@gmail.com>
2022-10-27 14:49:19 +0900

    1027

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import NavComp from "./components/NavComp";
+import NavbarComp from "./components/NavbarComp";
+import CardComp from "./components/CardComp";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan, faHeart as cleanHeart } from "@fortawesome/free-regular-svg-icons";
+import { faBagShopping, faChevronLeft, faChevronRight, faEnvelope, faHeart as fullHeart } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const settings = {
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComp />
+      <h1>제목</h1>
+      <CardComp />
+      <Button
+        as="a"
+        variant="primary"
+        onClick={() => {
+          alert("클릭");
+        }}
+      >
+        <FontAwesomeIcon icon={faEnvelope} />
+        <FontAwesomeIcon icon={faBagShopping} />
+        <FontAwesomeIcon icon={cleanHeart} />
+      </Button>
+      <Container>
+        <Row>
+          <Col xs>1 of 3</Col>
+          <Col>Variable width content</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col className="px-5">
+            <Slider {...settings}>
+              <div>
+                <h3>1</h3>
+              </div>
+              <div>
+                <h3>2</h3>
+              </div>
+              <div>
+                <h3>3</h3>
+              </div>
+              <div>
+                <h3>4</h3>
+              </div>
+              <div>
+                <h3>5</h3>
+              </div>
+              <div>
+                <h3>6</h3>
+              </div>
+            </Slider>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return <FontAwesomeIcon icon={fullHeart} onClick={onClick} className={className} style={{ color: "red" }} />;
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return <div className={className} style={{ ...style, display: "block", background: "green" }} onClick={onClick} />;
+}
---
+++ src/components/NavComp.jsx
@@ -0,0 +1,19 @@
+import Nav from "react-bootstrap/Nav";
+
+function NavComp() {
+  return (
+    <Nav activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
+      <Nav.Item>
+        <Nav.Link href="/home">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
+
+export default NavComp;
